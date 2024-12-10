65983a1f6122d7a1a60bab57f5d92335fb4be540
VioletEvergarden1017 <2606766964@qq.com>
2024-12-10 16:10:25 +0800

    Initial Commit

---
+++ ONE/ONEApp.swift
@@ -0,0 +1,17 @@
+//
+//  ONEApp.swift
+//  ONE
+//
+//  Created by 志野陶 on 2024/12/10.
+//
+
+import SwiftUI
+
+@main
+struct ONEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
